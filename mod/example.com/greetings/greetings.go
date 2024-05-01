84fed0d7c9fc311d0474be34eb5b6f5e600df918
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-05-01 13:37:22 +0200

    Add greetings-multiple-people tutorial

--- mod/example.com/greetings/greetings.go
+++ mod/example.com/greetings/greetings.go
@@ -29,6 +29,20 @@ func HelloRandom(name string) (string, error) {
 	return message, nil
 }
 
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := HelloRandom(name)
+		if err != nil {
+			return nil, err
+		}
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
 func randomFormat() string {
 	formats := []string{
 		"Hi, %v. Welcome!",
