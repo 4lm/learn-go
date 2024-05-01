8bd8616cb314a9503b779e9741c548b6df7dba70
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-05-01 13:13:41 +0200

    Add random-greeting tutorial

--- mod/example.com/greetings/greetings.go
+++ mod/example.com/greetings/greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) string {
@@ -17,4 +18,23 @@ func HelloWithError(name string) (string, error) {
 
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message, nil
+}
+
+func HelloRandom(name string) (string, error) {
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
