15ee37e92bac8f08043cb084a6541fb4687f047f
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-05-09 22:01:57 +0200

    Add assets

--- learning-path/05_lets-go/pasty/cmd/web/main.go
+++ learning-path/05_lets-go/pasty/cmd/web/main.go
@@ -6,16 +6,17 @@ import (
 )
 
 func main() {
-	const port string = ":4000"
-
 	mux := http.NewServeMux()
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /item/create", itemCreate)
 	mux.HandleFunc("POST /item/create", itemCreatePost)
 	mux.HandleFunc("GET /item/view/{id}", itemViewId)
 
+	const port string = ":4000"
 	log.Print("http://localhost" + port)
-
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 }
