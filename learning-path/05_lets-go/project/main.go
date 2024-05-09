4dca4bd1a8e106112d8745d903aee6c29dbcbd6e
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-05-09 16:25:42 +0200

    Add item create and view routes

--- learning-path/05_lets-go/project/main.go
+++ learning-path/05_lets-go/project/main.go
@@ -6,16 +6,27 @@ import (
 )
 
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", home)
-
 	const port string = ":4000"
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/item/create", itemCreate)
+	mux.HandleFunc("/item/view", itemView)
+
 	log.Print("http://localhost" + port)
+
 	err := http.ListenAndServe(port, mux)
 	log.Fatal(err)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Pasty"))
+}
+
+func itemCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Item / Create"))
+}
+
+func itemView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Item / View"))
 }
