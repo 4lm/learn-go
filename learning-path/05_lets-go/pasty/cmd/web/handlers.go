8dff21ca958742e4d8cbd0deb739dd8e4f9d60ad
Alexis Michaltsis <a.michaltsis@gmail.com>
2024-05-10 17:59:32 +0200

    Implement single record read

--- learning-path/05_lets-go/pasty/cmd/web/handlers.go
+++ learning-path/05_lets-go/pasty/cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"pasty.example.com/internal/models"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -53,5 +56,15 @@ func (app *application) snippetViewId(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Home / Snippet / View / %d - GET", id)
+	snippet, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%+v", snippet)
 }
